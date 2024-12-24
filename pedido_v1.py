b3816d8a07eb7844ef6047d2a1f26ee71044ef36
Pedro Victor <pedrovictor.rveras12@gmail.com>
2024-12-24 12:27:37 -0300

    Update pedido_v1.py

--- pedido_v1.py
+++ pedido_v1.py
@@ -113,7 +113,7 @@ class PedidoNamoro:
         self.card_image(
             "fotos/foto_2019_4.JPG",
             "Fim do carnaval com voce",
-            img_rotate= False
+            img_rotate= True
         )
 
         self.card_image(
@@ -136,6 +136,61 @@ class PedidoNamoro:
             img_rotate= False
         )
 
+        self.card_image(
+            "fotos/foto_2020_2.jpeg",
+            "Sempre sonhei ter uma pessoa que goste de entrar no mar comigo",
+            img_rotate= False
+        )
+
+        self.card_image(
+            "fotos/foto_2020_3.jpg",
+            "Sobrevivemos uma pandemia juntos...",
+            img_rotate= False
+        )
+
+
+        st.markdown("# 2021")
+
+        self.card_image(
+            "fotos/foto_2021_0.jpg",
+            "Tivemos mais uma virada de ano... de muitas emoçoes",
+            img_rotate= False
+        )
+
+        self.card_image(
+            "fotos/foto_2021_1.JPG",
+            "O tempo nos embelezou muito! Fala serio!!!",
+            img_rotate= True
+        )
+
+        self.card_image(
+            "fotos/foto_2021_3.jpg",
+            "Obrigado por sempre cuidar de mim <3 mesmo nas minhas dificuldades",
+            img_rotate= False
+        )
+
+        st.markdown("# 2024")
+
+        self.card_image(
+            "fotos/foto_2024_2.jpg",
+            "Ate hoje sou encantado com seu sorriso ilumindo o ambiente",
+            img_rotate= False
+        )
+
+        self.card_image(
+            "fotos/colagem.jpg",
+            "Obrigado por tudo nesse ano de 2024!",
+                        img_rotate= False
+
+        )
+
+        self.card_image(
+            "fotos/foto_2024.jpg",
+            "Ainda quero arrancar muito sorriso seu <3 ",
+            img_rotate= False
+        )
+
+
 
     def exibir_pedido(self):
         widgets.criar_markdown(
@@ -145,14 +200,14 @@ class PedidoNamoro:
 
         # Botão para exibir uma imagem
         widgets.criar_botao(
-            "Carta 1",
+            "Carta 1 (10/11/2024)",
             on_click=lambda: widgets.exibir_imagem(
                 ASSETS_PATH + "fotos/carta1.jpg", use_container_width=True
             ),
         )
 
         widgets.criar_botao(
-            "Carta 2",
+            "Carta 2 (18/12/2024)",
             on_click=lambda: widgets.exibir_imagem(
                 ASSETS_PATH + "fotos/carta2.jpg", use_container_width=True
             ),
@@ -245,7 +300,7 @@ class PedidoNamoro:
             widgets.criar_header(
                 "Para a mais especial, minha feiticeira, que tem o sorriso que ilumina meu coração, "
             )
-            widgets.criar_header("Danielle Serrano.")
+            widgets.criar_header("Feliz Natal, Danielle Serrano! :)", )
             st.divider()
             widgets.criar_subheader("🎶 Play na nossa musiquinha? 🎶")
             widgets.criar_subheader("1) Opção Natalina")
@@ -282,6 +337,37 @@ class PedidoNamoro:
             if self.dialog_message:
                 widgets.exibir_modal(self.dialog_message)
                 self.dialog_message = None  # Limpar a mensagem após exibir
+
+            st.divider()
+            natal_css = """
+            /* customizable snowflake styling */
+            .snowflake {
+            color: #FFF;
+            font-size: 1em;
+            font-family: Arial;
+            text-shadow: 0 0 1px #000;
+            }
+
+            @-webkit-keyframes snowflakes-fall{0%{top:-10%}100%{top:100%}}@-webkit-keyframes snowflakes-shake{0%{-webkit-transform:translateX(0px);transform:translateX(0px)}50%{-webkit-transform:translateX(80px);transform:translateX(80px)}100%{-webkit-transform:translateX(0px);transform:translateX(0px)}}@keyframes snowflakes-fall{0%{top:-10%}100%{top:100%}}@keyframes snowflakes-shake{0%{transform:translateX(0px)}50%{transform:translateX(80px)}100%{transform:translateX(0px)}}.snowflake{position:fixed;top:-10%;z-index:9999;-webkit-user-select:none;-moz-user-select:none;-ms-user-select:none;user-select:none;cursor:default;-webkit-animation-name:snowflakes-fall,snowflakes-shake;-webkit-animation-duration:10s,3s;-webkit-animation-timing-function:linear,ease-in-out;-webkit-animation-iteration-count:infinite,infinite;-webkit-animation-play-state:running,running;animation-name:snowflakes-fall,snowflakes-shake;animation-duration:10s,3s;animation-timing-function:linear,ease-in-out;animation-iteration-count:infinite,infinite;animation-play-state:running,running}.snowflake:nth-of-type(0){left:1%;-webkit-animation-delay:0s,0s;animation-delay:0s,0s}.snowflake:nth-of-type(1){left:10%;-webkit-animation-delay:1s,1s;animation-delay:1s,1s}.snowflake:nth-of-type(2){left:20%;-webkit-animation-delay:6s,.5s;animation-delay:6s,.5s}.snowflake:nth-of-type(3){left:30%;-webkit-animation-delay:4s,2s;animation-delay:4s,2s}.snowflake:nth-of-type(4){left:40%;-webkit-animation-delay:2s,2s;animation-delay:2s,2s}.snowflake:nth-of-type(5){left:50%;-webkit-animation-delay:8s,3s;animation-delay:8s,3s}.snowflake:nth-of-type(6){left:60%;-webkit-animation-delay:6s,2s;animation-delay:6s,2s}.snowflake:nth-of-type(7){left:70%;-webkit-animation-delay:2.5s,1s;animation-delay:2.5s,1s}.snowflake:nth-of-type(8){left:80%;-webkit-animation-delay:1s,0s;animation-delay:1s,0s}.snowflake:nth-of-type(9){left:90%;-webkit-animation-delay:3s,1.5s;animation-delay:3s,1.5s}
+
+                """
+
+            # Injetando CSS no app
+            st.markdown(
+                f"<style>{natal_css}</style>", unsafe_allow_html=True
+            )
+
+            # Criando flocos de neve
+            for i in range(20):  # Número de flocos de neve
+                animation_symbol = "❄️"
+                st.markdown(
+                    f"""
+                    <div class="snowflake" style="left: {i * 10}%; -webkit-animation-delay: {i * 0.5}s; animation-delay: {i * 0.5}s;">{animation_symbol}</div>
+                    """,
+                    unsafe_allow_html=True
+                )
+            widgets.criar_header("Agradecimentos, Feliz Natal! e um prospero ano de 2025")
+            widgets.criar_header("Pedro Victor")
 
 
 if __name__ == "__main__":
